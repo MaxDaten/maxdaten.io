5c889098d746e3be8f1cd43963a2b1d5815ef385
Jan-Philip Loos <maxdaten@gmail.com>
2024-03-29 02:49:32 +0100

    Rotate gems array based on the current day

--- src/routes/(waves)/gems/+page.server.ts
+++ src/routes/(waves)/gems/+page.server.ts
@@ -1,7 +1,13 @@
 import { gems } from '$lib/data/gems';
 
 export async function load() {
+	const day = new Date().getDay();
 	return {
-		gems: gems
+		gems: rotateArray(gems, day)
 	};
+}
+
+function rotateArray(arr: [], count: number) {
+	const clippedCount = count % arr.length;
+	return arr.slice(clippedCount).concat(arr.slice(0, clippedCount));
 }
