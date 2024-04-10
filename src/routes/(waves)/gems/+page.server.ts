075561902ec1c0bc5c771fd9fb6d97c36f86727b
Jan-Philip Loos <maxdaten@gmail.com>
2024-04-10 19:43:47 +0200

    Update cache-control header in gems page server

--- src/routes/(waves)/gems/+page.server.ts
+++ src/routes/(waves)/gems/+page.server.ts
@@ -1,7 +1,12 @@
 import { gems } from '$lib/data/gems';
 
-export async function load() {
+export async function load({ setHeaders }) {
 	const day = new Date().getDay();
+
+	setHeaders({
+		// 12 hours
+		'cache-control': 'public, max-age=43200'
+	});
 	return {
 		gems: rotateArray(gems, day)
 	};
@@ -9,5 +14,5 @@ export async function load() {
 
 function rotateArray<T>(arr: T[], count: number) {
 	const clippedCount = count % arr.length;
-	return arr.slice(clippedCount).concat(arr.slice(0, clippedCount));
+	return arr.slice(clippedCount, arr.length).concat(arr.slice(0, clippedCount));
 }
