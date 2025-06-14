c44ac7cbda7e6abbb00eeb29ae8a83962cd3f2ec
Jan-Philip Loos <maxdaten@gmail.com>
2025-06-15 00:54:46 +0200

    fix: BlogPostCard.svelte not rendering image and content correctly

--- src/lib/components/molecules/BlogPostCard.svelte
+++ src/lib/components/molecules/BlogPostCard.svelte
@@ -12,7 +12,7 @@
 	let {
 		post,
 		showImage = true
-	}: Props = $props()
+	}: Props = $props();
 </script>
 
 <Card
@@ -21,55 +21,42 @@
 	additionalClass="blog-post-card {!showImage || !post.coverImage ? 'no-image' : ''}"
 >
 	{#snippet image()}
-		<div class="image">
-			{#if post.coverImage}
-				<Image src={post.coverImage} alt="Cover image of this blog post" />
-			{/if}
-		</div>
+		{#if post.coverImage}
+			<Image src={post.coverImage} alt="Cover image of this blog post" />
+		{/if}
 	{/snippet}
 	{#snippet content()}
-		<div class="content">
-			<p class="title">
-				{post.title}
+		<p class="title">
+			{post.title}
+		</p>
+		{#if post.readingTimeMinutes}
+			<div class="note">{post.readingTimeMinutes} min read</div>
+		{/if}
+		{#if post.excerpt}
+			<p class="text">
+				{post.excerpt}
 			</p>
-			{#if post.readingTimeMinutes}
-				<div class="note">{post.readingTimeMinutes} min read</div>
-			{/if}
-			{#if post.excerpt}
-				<p class="text">
-					{post.excerpt}
-				</p>
-			{/if}
-		</div>
+		{/if}
 	{/snippet}
 	{#snippet footer()}
-		<div class="footer">
-			{#if post.tags?.length}
-				<div class="tags">
-					{#each post.tags.slice(0, 2) as tag}
-						<Tag>{tag}</Tag>
-					{/each}
-				</div>
-			{/if}
-		</div>
+		{#if post.tags?.length}
+			<div class="tags">
+				{#each post.tags.slice(0, 2) as tag}
+					<Tag>{tag}</Tag>
+				{/each}
+			</div>
+		{/if}
 	{/snippet}
 </Card>
 
 <style lang="scss">
-  .content {
-    display: flex;
-    flex-direction: column;
-    gap: 0;
-    align-items: flex-start;
-  }
-
   .title {
     display: flex;
     align-items: center;
     justify-content: space-between;
     width: 100%;
     font-size: 1.2rem;
-    font-family: var(--font--title),serif;
+    font-family: var(--font--title), serif;
     font-weight: 700;
   }
 
@@ -89,10 +76,6 @@
     margin-top: 5px;
     font-size: 0.9rem;
     text-align: justify;
-  }
-
-  .footer {
-    margin-top: 20px;
   }
 
   :global(.blog-post-card .image img) {
