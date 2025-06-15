e39aefa9a465a1f858ca8cc777088d54f162fe7b
Jan-Philip Loos <maxdaten@gmail.com>
2025-06-15 03:23:12 +0200

    chore: add `Analytics` component to layout for global tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '$lib/scss/global.scss';
+	import Analytics from '$components/atoms/Analytics.svelte';
 	/**
 	 * @typedef {Object} Props
 	 * @property {import('svelte').Snippet} [children]
@@ -9,4 +10,5 @@
 	let { children } = $props();
 </script>
 
+<Analytics/>
 {@render children?.()}
