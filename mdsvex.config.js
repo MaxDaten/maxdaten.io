93c283d97ddd75704a37882b2806bd708d6f78d5
Jan-Philip Loos <maxdaten@gmail.com>
2025-07-12 23:54:57 +0200

    refactor: replace BlogArticleWrapper.svelte with MdsvexWrapper.svelte and integrate shiki for syntax highlighting

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,19 +1,35 @@
 import rehypeExternalLinks from 'rehype-external-links';
 import rehypeSlug from 'rehype-slug';
+import { createHighlighter } from 'shiki';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { escapeSvelte } from 'mdsvex';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const theme = 'ayu-dark';
+const highlighter = await createHighlighter({
+    themes: [theme],
+    langs: ['html', 'css', 'js', 'ts', 'hcl', 'nix', 'yaml', 'bash', 'text'],
+});
 
 /** @type import("mdsvex").MdsvexOptions */
 const config = {
     layout: join(
         __dirname,
-        'src/lib/components/organisms/BlogArticleWrapper.svelte',
+        'src/lib/components/organisms/MdsvexWrapper.svelte',
     ),
     extensions: ['.svx', '.md'],
+    highlight: {
+        highlighter: async (code, lang = 'text') => {
+            const html = escapeSvelte(
+                highlighter.codeToHtml(code, { lang, theme }),
+            );
+            return `<Components.CodeBlock lang="${lang}">{@html \`${html}\`}</Components.CodeBlock>`;
+        },
+    },
     rehypePlugins: [
         rehypeExternalLinks, // Adds 'target' and 'rel' to external links
         rehypeSlug, // Adds 'id' attributes to Headings (h1,h2,etc)
---
+++ src/lib/components/organisms/MdsvexWrapper.svelte
@@ -0,0 +1,21 @@
+<!--suppress JSUnusedGlobalSymbols -->
+<script module lang="ts">
+    // components available for a blog article, can be used in rehype
+    export { default as Callout } from '$components/molecules/Callout.svelte';
+    export { default as CodeBlock } from '$components/molecules/CodeBlock.svelte';
+    export { default as Image } from '$components/atoms/Image.svelte';
+</script>
+
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import type { BlogPost } from '$utils/types';
+
+    interface Props {
+        post: BlogPost;
+        children: Snippet;
+    }
+
+    let { post: _, children }: Props = $props();
+</script>
+
+{@render children()}
