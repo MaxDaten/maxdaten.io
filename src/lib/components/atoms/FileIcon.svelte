212a2bc08b5e06c88caabc7a74962053161f99a6
Jan-Philip Loos <maxdaten@gmail.com>
2025-07-14 23:39:04 +0200

    chore: update `.editorconfig` with Markdown-specific indentation rules
    - Set `indent_size` to 4 and `indent_style` to space for Markdown files

--- src/lib/components/atoms/FileIcon.svelte
+++ src/lib/components/atoms/FileIcon.svelte
@@ -1,34 +1,38 @@
 <script lang="ts">
-	import { getFileIcon } from '$lib/utils/fileIcons.js';
+    /* eslint-disable svelte/no-at-html-tags */
+    import { getFileIcon } from '$lib/utils/fileIcons.js';
 
-	interface Props {
-		lang: string | null;
-		size?: number;
-		class?: string;
-	}
+    interface Props {
+        lang: string | null;
+        size?: number;
+        class?: string;
+    }
 
-	let { lang, size = 16, class: className = '' }: Props = $props();
+    let { lang, size = 16, class: className = '' }: Props = $props();
 
-	const iconSvg = $derived(getFileIcon(lang));
+    const iconSvg = $derived(getFileIcon(lang));
 </script>
 
 {#if iconSvg}
-	<span class="file-icon {className}" style="width: {size}px; height: {size}px;">
-		{@html iconSvg}
-	</span>
+    <span
+        class="file-icon {className}"
+        style="width: {size}px; height: {size}px;"
+    >
+        {@html iconSvg}
+    </span>
 {/if}
 
 <style>
-	.file-icon {
-		display: inline-flex;
-		align-items: center;
-		justify-content: center;
-		flex-shrink: 0;
-	}
+    .file-icon {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        flex-shrink: 0;
+    }
 
-	.file-icon :global(svg) {
-		width: 100%;
-		height: 100%;
-		display: block;
-	}
+    .file-icon :global(svg) {
+        width: 100%;
+        height: 100%;
+        display: block;
+    }
 </style>
