6b306fb2495c504bc2b268b36be63cba1afc18be
Jan-Philip Loos <maxdaten@gmail.com>
2025-07-16 00:36:41 +0200

    chore: update `treefmt.nix` with expanded Prettier configuration and additional file type support

--- treefmt.nix
+++ treefmt.nix
@@ -1,9 +1,34 @@
 # https://github.com/numtide/treefmt-nix#flakes
-{ pkgs, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   # Used to find the project root
   projectRootFile = "flake.nix";
   programs.nixfmt.enable = true;
   programs.nixfmt.package = pkgs.nixfmt-rfc-style;
   programs.prettier.enable = true;
+  programs.prettier.settings = builtins.fromJSON (lib.readFile ./.prettierrc);
+  programs.prettier.includes = [
+    "*.cjs"
+    "*.css"
+    "*.html"
+    "*.js"
+    "*.json"
+    "*.json5"
+    "*.jsx"
+    "*.md"
+    "*.mdx"
+    "*.mjs"
+    "*.scss"
+    "*.ts"
+    "*.tsx"
+    "*.vue"
+    "*.yaml"
+    "*.yml"
+    "*.svelte"
+  ];
 }
