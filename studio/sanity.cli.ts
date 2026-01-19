33cee754a1db48f9acd61c976d4d49ccf7ac65f8
Jan-Philip Loos <maxdaten@gmail.com>
2026-01-19 04:26:59 +0100

    chore: Remove deprecated studioHost field
    Replaced by deployment.appId

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,7 +5,6 @@ export default defineCliConfig({
         projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'your-project-id',
         dataset: process.env.SANITY_STUDIO_DATASET || 'production',
     },
-    studioHost: 'maxdaten',
     deployment: {
         appId: 'b51mlm1n98z4561inucc06y5',
     },
